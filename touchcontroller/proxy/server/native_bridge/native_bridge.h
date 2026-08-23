e2f5670da7bd5b74af4a04bf16d12279942c4b42
fifth_light <liu-yu-jie@foxmail.com>
2026-08-23 22:20:46 +0800

    [TouchController] 格式化原生桥的 C 头文件

--- touchcontroller/proxy/server/native_bridge/native_bridge.h
+++ touchcontroller/proxy/server/native_bridge/native_bridge.h
@@ -3,21 +3,19 @@
 
 #include <jni.h>
 
-JNIEXPORT void JNICALL Java_top_fifthlight_touchcontroller_common_platform_nativebridge_Interface_init(JNIEnv* env,
-                                                                                                 jclass clazz,
-                                                                                                 jlong art_jvm_ptr,
-                                                                                                 jlong art_application);
+JNIEXPORT void JNICALL Java_top_fifthlight_touchcontroller_common_platform_nativebridge_Interface_init(
+    JNIEnv* env, jclass clazz, jlong art_jvm_ptr, jlong art_application);
 
 JNIEXPORT void JNICALL Java_top_fifthlight_touchcontroller_common_platform_nativebridge_Interface_send(JNIEnv* env,
-                                                                                                 jclass clazz,
-                                                                                                 jbyteArray data,
-                                                                                                 jint len);
+                                                                                                       jclass clazz,
+                                                                                                       jbyteArray data,
+                                                                                                       jint len);
 
 JNIEXPORT jint JNICALL Java_top_fifthlight_touchcontroller_common_platform_nativebridge_Interface_poll(JNIEnv* env,
-                                                                                                 jclass clazz,
-                                                                                                 jbyteArray buf);
+                                                                                                       jclass clazz,
+                                                                                                       jbyteArray buf);
 
 JNIEXPORT void JNICALL Java_top_fifthlight_touchcontroller_common_platform_nativebridge_Interface_stop(JNIEnv* env,
-                                                                                                 jclass clazz);
+                                                                                                       jclass clazz);
 
 #endif
