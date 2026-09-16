58c20323a604de03827b7aa9a82c1212ee1591d6
fifth_light <liu-yu-jie@foxmail.com>
2026-09-16 12:55:37 +0800

    [TouchController] Fix 1.21.1 crash when opening vanilla chat screen

--- touchcontroller/versions/mixin/1.21.1/MinecraftMixin.java
+++ touchcontroller/versions/mixin/1.21.1/MinecraftMixin.java
@@ -7,19 +7,29 @@ package top.fifthlight.touchcontroller.mixin.v1_21_1;
 
 import net.minecraft.client.Minecraft;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import top.fifthlight.touchcontroller.common.event.render.RenderEvents;
 import top.fifthlight.touchcontroller.common.model.TouchControllerLoadStatus;
+import top.fifthlight.touchcontroller.extension.v1_21_1.ChatScreenOpenable;
 
 @Mixin(Minecraft.class)
-public abstract class MinecraftMixin {
+public abstract class MinecraftMixin implements ChatScreenOpenable {
+    @Shadow
+    protected abstract void openChatScreen(String par1);
+
     @Inject(method = "runTick", at = @At("HEAD"))
     public void onRenderStart(boolean renderLevel, CallbackInfo ci) {
         var instance = TouchControllerLoadStatus.INSTANCE;
         if (instance.isLoaded()) {
             RenderEvents.onRenderStart();
         }
+    }
+
+    @Override
+    public void touchcontroller$openChatScreen(String text) {
+        this.openChatScreen(text);
     }
 }
